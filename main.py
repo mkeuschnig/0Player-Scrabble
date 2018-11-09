94c525b7fc62f0510535bedeb1a2f8d92929c521
Markus Keuschnig <MarkusKeuschnig@msn.com>
2018-11-09 17:42:47 +0100

    Transport

--- main.py
+++ main.py
@@ -1,88 +1,49 @@
+#1234567890123456789012345678901234567890123456789012345678901234567890123456789
 # contains the main game-loop and settings
 
 # import general modules
-import random
+from random import randint
 import re 
 import shelve
 #from copy import deepcopy
-from sys import path
+from sys import path as OSP
+#import importlib
 
 
-#DEBUG: insert working directory in path to access the Scrabble-specific files
+# DEBUG: insert working directory in path to access the Scrabble-specific files
 # TODO: work out proper path
-##osp.insert(0, 'E:/Projekte/0Player-Scrabble/')
-path.insert(-1, "D:\\Projekte\\Scrabble Solver\\0Player-Scrabble\\")
+OSP.insert(0, 'E:\\Projekte\\0Player-Scrabble\\')
+#path.insert(-1, "D:\\Projekte\\Scrabble Solver\\0Player-Scrabble\\")
 
-#TODO: check if the other modules exist. OS.path
+#TODO: check if the other modules exist. OS.path or sys.path
 #http://effbot.org/librarybook/os-path.htm
 #appPath = osp.getabspath()
 
-#DEBUG
+# DEBUG:
 import pprint
 
-# import scrabble-specific source files
+# import scrabble-specific source files as global
+global S, C, L, D, ScrabbleLogging
 import settings as S
 import checks as C
 import logic as L
 import display as D
 import logging as ScrabbleLogging
 
+# DEBUG:
+import unit_tests as UT
 
 
 # ***GLOBALS***
+# TODO: Make proper Game-Settings in a dict
+global GAMESETTINGS
 GAMESETTINGS = list(S.getGameSettings())
 
 
-# list to hold the letters from the bag
-RACK = []
-
-
-# Pseudo Unit-Testing/Sanity-Checks:
 #pprint.pprint(GAMESETTINGS)
 print(GAMESETTINGS)
 
-# checks.py
-C.checkIsPositionValid("A1") is True
-C.checkIsPositionValid("O15") is True
-C.checkIsPositionValid("B52") is False
-C.checkIsPositionValid("Z6") is False
 
-C.checkIsCoordinateValid(0,0) is True
-C.checkIsCoordinateValid(99,99) is False
-C.checkIsCoordinateValid(75,0) is False
-C.checkIsCoordinateValid(-1,-1) is False
-
-# logic.py
-list(L.findIndexesOfLetterInWord("A", "ABRACADABRA")) == [0,3,5,7,10]
-list(L.findIndexesOfLetterInWord("Z", "BANANA")) == []
-
-L.convertCoordinateToPosition(0, 0) == "A1"
-L.convertCoordinateToPosition(14, 14) == "O15"
-L.convertCoordinateToPosition(62, 52) is None
-L.convertCoordinateToPosition(2, 52) is None
-L.convertCoordinateToPosition(52, 2) is None
-
-L.convertPositionToCoordinate("A1") == (0, 0)
-L.convertPositionToCoordinate("O15") == (14, 14)
-L.convertPositionToCoordinate("Ü92") == (None, None)
-L.convertPositionToCoordinate("A92") == (None, None)
-L.convertPositionToCoordinate("Z6") == (None, None)
-
-L.getModifiedPosition("H8", -7, -7) == "A1"
-L.getModifiedPosition("A1", +7, +7) == "H8"
-L.getModifiedPosition("O15", -14, -14) == "A1"
-L.getModifiedPosition("H8", -10, -10) is None
-L.getModifiedPosition("B1", -4, -4) is None
-
-L.convertPositionsToList("A1", "A5") == ["A1", "A2", "A3", "A4", "A5"]
-L.convertPositionsToList("A1", "A1") == ["A1"]
-L.convertPositionsToList("A1") == ["A1"]
-L.convertPositionsToList("A1", "B2") == [] 
-
-L.set
-
-
-
-
-
+# Pseudo Unit-Testing/Sanity-Checks: 
+UT.unit_tests()
 
---
+++ unit_tests.py
@@ -0,0 +1,166 @@
+import settings as S
+import checks as C
+import logic as L
+
+def unit_tests():
+    # checks.py
+
+    assert C.checkIsPositionValid("A1") is True
+    assert C.checkIsPositionValid("O15") is True
+    assert C.checkIsPositionValid("B52") is False
+    assert C.checkIsPositionValid("Z6") is False
+
+    assert C.checkIsCoordinateValid(0,0) is True
+    assert C.checkIsCoordinateValid(99,99) is False
+    assert C.checkIsCoordinateValid(75,0) is False
+    assert C.checkIsCoordinateValid(-1,-1) is False
+
+    # logic.py
+    assert list(L.findIndexesOfLetterInWord("A", "ABRACADABRA")) == [0,3,5,7,10]
+    assert list(L.findIndexesOfLetterInWord("Z", "BANANA")) == []
+
+    assert L.convertCoordinateToPosition(0, 0) == "A1"
+    assert L.convertCoordinateToPosition(14, 14) == "O15"
+    assert L.convertCoordinateToPosition(62, 52) is None
+    assert L.convertCoordinateToPosition(2, 52) is None
+    assert L.convertCoordinateToPosition(52, 2) is None
+
+    assert L.convertPositionToCoordinate("A1") == (0, 0)
+    assert L.convertPositionToCoordinate("O15") == (14, 14)
+    assert L.convertPositionToCoordinate("Ö92") == (None, None)
+    assert L.convertPositionToCoordinate("A92") == (None, None)
+    assert L.convertPositionToCoordinate("Z6") == (None, None)
+
+    assert L.getModifiedPosition("H8", -7, -7) == "A1"
+    assert L.getModifiedPosition("A1", +7, +7) == "H8"
+    assert L.getModifiedPosition("O15", -14, -14) == "A1"
+    assert L.getModifiedPosition("H8", -10, -10) is None
+    assert L.getModifiedPosition("B1", -4, -4) is None
+
+    assert L.convertPositionsToList("A1", "A5") == ["A1", "A2", "A3", "A4", "A5"]
+    assert L.convertPositionsToList("A1", "F1") == ["A1", "B1", "C1", 
+                                                    "D1", "E1", "F1"]
+    assert L.convertPositionsToList("A1", "A1") == ["A1"]
+    assert L.convertPositionsToList("A1") == ["A1"]
+    assert L.convertPositionsToList("A1", "B2") == [] 
+
+    # Place "ERNST" on actual board
+
+    L.setLetterToPosition("A1", "E")
+    L.setLetterToPosition("B1", "R")
+    L.setLetterToPosition("C1", "N")
+    L.setLetterToPosition("D1", "S")
+    L.setLetterToPosition("E1", "T")
+
+    w = ""
+    for currentPosition in L.convertPositionsToList("A1", "E1"):
+        w += "".join(L.getLetterFromPosition(currentPosition, 
+                                                isTemporary = False))
+    print(w)
+    assert w == "ERNST"
+
+    # Place "TEMPOR�R" on temporary board
+    # global recent temp. Positions are added automatically.
+    L.setLetterToPosition("A1", "T", isTemporary = True)
+    L.setLetterToPosition("B1", "E", isTemporary = True)
+    L.setLetterToPosition("C1", "M", isTemporary = True)
+    L.setLetterToPosition("D1", "P", isTemporary = True)
+    L.setLetterToPosition("E1", "O", isTemporary = True)
+    L.setLetterToPosition("F1", "R", isTemporary = True)
+    L.setLetterToPosition("G1", "Ä", isTemporary = True)
+    L.setLetterToPosition("H1", "R", isTemporary = True)
+
+    w = ""
+    for currentPosition in L.convertPositionsToList("A1", "H1"):
+        w += "".join(L.getLetterFromPosition(currentPosition, 
+                                             isTemporary = True))
+    print(w)
+    w == "TEMPORÄR"
+
+    # get filled positions from both boards in various ways.
+    assert L.getFilledPositionList("A1") == ["A1"]
+    assert L.getFilledPositionList("A1","A0") == []
+    assert L.getFilledPositionList("A1", entireAxis = "X")
+    assert L.getFilledPositionList("A1", entireAxis = "Y")
+    assert L.getFilledPositionList("A1", 
+                            entireAxis = "X",
+                            isTemporary = True)
+    assert L.getFilledPositionList("A1", 
+                            entireAxis = "Y",
+                            isTemporary = True)
+    assert L.getFilledPositionList("A1", 
+                            entireAxis = "X",
+                            isTemporary = True,
+                            returnLetters = True)
+    assert L.getFilledPositionList("A1", 
+                            entireAxis = "Y",
+                            isTemporary = True,
+                            returnLetters = True)
+    assert L.getFilledPositionList(startPosition = "A1", 
+                            isTemporary = True,
+                            returnLetters = True,
+                            entireAxis = "X") == list("TEMPORÄR")
+    assert L.getFilledPositionList("A1", "E1", 
+                            returnLetters = True) == list("ERNST")
+    assert L.getFilledPositionList("A1", "A10", returnLetters = True) == ["E"]
+    assert L.getFilledPositionList("A1", "A10", returnLetters = True) == ["E"]
+    assert L.getFilledPositionList("A1", "A10", 
+                            returnLetters = True, entireAxis = "X") == list("ERNST")
+
+    # see if "TEMPOR�R" is still in the global variable.
+    w = ""
+    for tempPosition in S.RECENT_POSITIONS_TEMPORARY:
+        w += ''.join(L.getLetterFromPosition(tempPosition, isTemporary = True))
+    print(w)
+
+    # clear the recent temporary positions.
+    assert len(S.RECENT_POSITIONS_TEMPORARY) > 0
+    L.clearAllTemporaryPositions()
+    assert len(S.RECENT_POSITIONS_TEMPORARY) == 0
+    # check if everything on the temporary Board is empty.
+    assert L.getFilledPositionList("A1", isTemporary = True, entireAxis = "X") == []
+    assert L.getFilledPositionList("A1", isTemporary = True, entireAxis = "Y") == []
+
+    # getWordFromPosition
+    assert L.getWordFromPosition("A1","E1") == "ERNST"
+    assert L.getWordFromPosition("A1","A15") == "E"
+    assert L.getWordFromPosition("A0","B12") == ""
+
+    # modifier-fields
+    assert L.getWordModifier("A1", "A15") == 27
+    assert L.getWordModifier("A1", "O1") == 27
+    assert L.getWordModifier("A1", "A8") == 9
+    assert L.getWordModifier("A1") == 3
+    assert L.getWordModifier("A2") == 1
+    assert L.getWordModifier("B2", "N2") == 4
+    assert L.getWordModifier("B6") == 1
+
+    assert L.getLetterModifier("B2") == 1
+    assert L.getLetterModifier("F2") == 3
+    assert L.getLetterModifier("B6") == 3
+    assert L.getLetterModifier("B1") == 1
+
+    # set entire words:
+    L.setWordToPosition("ERNST","A2","E2")
+    assert L.getWordFromPosition("A2","E2") == "ERNST"
+
+    L.setWordToPosition("ER?ST","A3","E3",
+                        jokerReplacement = "N")
+    assert L.getWordFromPosition("A3","E3", showJoker = True) == "ER?ST"
+
+    L.setWordToPosition("ER??T","A4","E4",
+                        jokerReplacement = "NS")
+    assert L.getWordFromPosition("A4","E4", showJoker = True) == "ER??T"
+
+    L.setWordToPosition("?????","A5","E5",
+                        jokerReplacement = "ERNST")
+    assert L.getWordFromPosition("A5","E5", showJoker = True) == "?????"
+    assert L.getWordFromPosition("A5","E5", showJoker = False) == "ERNST"
+
+
+    L.setWordToPosition("EINS","A8","O8","X")
+    assert L.getWordFromPosition("A8","O8") == "EINS"
+
+    L.getLetterFromPosition("A5", showJoker = False) == "E"
+    L.getLetterFromPosition("A5", showJoker = True) == "?E"
+# end of unit_tests()
